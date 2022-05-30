4420c4ceb6aa57f3db5246e7240584ab2266c009
nik-trifonov <102224540+nik-trifonov@users.noreply.github.com>
2022-05-30 22:17:51 +0300

    Add files via upload

---
+++ Summator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summator_Proeject
+{
+    public class Summator
+    {
+        public static int Sum(int[] arr)
+        {
+            int sum = arr[0];
+            for(int i=1;i<arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+        public static int Average(int[] arr)
+        {
+            int sum = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum/arr.Length;
+        }
+
+
+    }
+
+}
